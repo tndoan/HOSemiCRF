c08eb179bb40c24fe512cefa957570f48c0d5c7d
tndoan <doan.thanh.nam.1988@gmail.com>
2015-09-07 16:09:08 +0800

    add batch update

--- src/optimization/SVRGMinimizer.java
+++ src/optimization/SVRGMinimizer.java
@@ -75,4 +75,88 @@ public class SVRGMinimizer extends Minimizer {
 		
 		return w_0_tilde;
 	}
+	
+	/**
+	 * This function is the experience function of using 
+	 * @param f
+	 * @param init
+	 * @param learningRate
+	 * @param numRandom		number of random indices which are selected in the inner loop
+	 * @param maxPasses
+	 * @param funcTol
+	 * @return
+	 */
+	public double[] minimize(AbstractSVRGFunction f, double[] init,
+			double learningRate, int numRandom, int maxPasses, double funcTol){
+		int n = f.getNumberOfDataPoints(); // number of data points
+		int upFreq = 10;
+		int d = init.length;
+
+		double[] w_0_tilde = new double[d];
+		double[] mu_tilde; // = new double[d];
+		double[] w_tilde = new double[d];
+		double[] w_0 = new double[d];
+
+		Random rand = new Random(123456987);
+		
+		// copy init value
+		System.arraycopy(init, 0, w_0_tilde, 0, d);
+		
+		((FirstOrderDiffFunction)f).computeAllValues(w_0_tilde);
+		double pre_obj = f.valueAt(w_0_tilde);
+		double[][] eachDerivatives = f.takeEachDerivative(w_0_tilde);
+		mu_tilde = f.takeDerivative(w_0_tilde);
+		
+		int[] i_t = new int[numRandom];
+		double nR = (double) numRandom;
+		
+		for (int s = 0; s < maxPasses; s++){
+			// w_tilde = w_0_tilde
+			System.arraycopy(w_0_tilde, 0, w_tilde, 0, d);
+			
+			// mu_tilde = (\sum_{i=1}^n \delta \psi_i (w_tilde)) / n			
+			for (int i = 0; i < d; i++){				
+				mu_tilde[i] /= (double) n;
+			}
+			
+			//w_0 = w_tilde
+			System.arraycopy(w_tilde, 0, w_0, 0, d);
+			
+			for (int t = 0; t < upFreq; t++){
+				for (int i = 0; i < numRandom; i++)
+					i_t[i] = rand.nextInt(n);
+				
+				double[] f1 = f.takeDerivative(w_0, i_t);
+				double[] f2 = new double[d];
+				
+				for (int i = 0; i < d; i++)
+					for (int j = 0; j < numRandom; j++)
+						f2[i] += eachDerivatives[i_t[j]][i] ;
+
+				for (int i = 0; i < d; i++){
+					w_0[i] = w_0[i] - learningRate * (f1[i] / nR - f2[i] / nR + mu_tilde[i]);
+				}
+			}
+			
+			// w_0_tilde = w_0
+			System.arraycopy(w_0, 0, w_0_tilde, 0, d);
+			
+			// check convergence
+			((FirstOrderDiffFunction)f).computeAllValues(w_0_tilde);
+			double curr_obj = f.valueAt(w_0_tilde);
+			mu_tilde = f.takeDerivative(w_0_tilde);
+			eachDerivatives = f.takeEachDerivative(w_0_tilde);
+			
+			double diff = Math.abs(curr_obj - pre_obj);
+			if (s > 1 && diff < funcTol) {
+				// s > 1 to ensure that objective function is calculated at least 1 time.
+				this.isConv = true;
+				break;
+			}
+			System.out.println("Objective function pre:" + pre_obj + " curr:" + curr_obj + " funcTol:" + funcTol);
+			pre_obj = curr_obj;
+		}
+		
+		return w_0_tilde;
+	}
 }
